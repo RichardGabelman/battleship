7c48f55a0c4d88c163ee4bd6f39cc107f39fea2d
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2024-08-28 14:26:19 -0700

    Implement placeShip function

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,4 +1,4 @@
-const BOARD_SIZE = 10;
+export const BOARD_SIZE = 10;
 
 export class Gameboard {
   #board;
@@ -19,5 +19,44 @@ export class Gameboard {
       copy[i] = [...this.#board[i]];
     }
     return copy;
+  }
+
+  // returns true if ship was successfully placed
+  // coords are inclusive
+  // ex. [0, 0] && [0, 2]
+  // [[🚢, 🚢, 🚢, ...]]
+  placeShip(coordOne, coordTwo) {
+    const [y1, x1] = coordOne;
+    const [y2, x2] = coordTwo;
+
+    // no placing ships outside of the board
+    if (y1 < 0 || y2 < 0 || x1 < 0 || x2 < 0) {
+      return false;
+    }
+    if (y1 >= BOARD_SIZE || y2 >= BOARD_SIZE || x1 >= BOARD_SIZE || x2 >= BOARD_SIZE) {
+      return false;
+    }
+
+    // no placing ships diagonally
+    if (y1 != y2 && x1 != x2) {
+      return false;
+    }
+
+    const backupBoard = this.board;
+    const minY = Math.min(y1, y2);
+    const minX = Math.min(x1, x2);
+    const maxY = Math.max(y1, y2);
+    const maxX = Math.max(x1, x2);
+
+    for (let i = minY; i <= maxY; i++) {
+      for (let j = minX; j <= maxX; j++) {
+        if (this.#board[i][j] === "🚢") {
+          this.#board = backupBoard;
+          return false;
+        }
+        this.#board[i][j] = "🚢";
+      }
+    }
+    return true;
   }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,4 +1,4 @@
-import { Gameboard } from "../modules/gameboard.js";
+import { Gameboard, BOARD_SIZE } from "../modules/gameboard.js";
 
 describe("get board()", () => {
   test("new Gameboard contains 'empty' board", () => {
@@ -19,7 +19,53 @@ describe("get board()", () => {
   test("mutations to gotten board don't affect actual board", () => {
     const testBoard = new Gameboard();
     let copyOfBoard = testBoard.board;
-    copyOfBoard[0][0] = "⛵"
+    copyOfBoard[0][0] = "⛵";
     expect(testBoard.board[0][0]).toEqual("🌊");
-  })
+  });
+});
+
+describe("placeShip()", () => {
+  test("placed ship is correctly represented on the board", () => {
+    const testBoard = new Gameboard();
+    testBoard.placeShip([0,0], [0,4]);
+    expect(testBoard.board).toEqual([
+      ["🚢", "🚢", "🚢", "🚢", "🚢", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"],
+      ["🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊"]
+    ]);
+  });
+  test("ships cannot be placed if any of the ship would be outside the gameboard", () => {
+    const testBoard = new Gameboard();
+    expect(testBoard.placeShip([-1, 0], [0, 0])).toEqual(false);
+    expect(testBoard.placeShip([0, -1], [0, 0])).toEqual(false);
+    expect(testBoard.placeShip([BOARD_SIZE, 0], [BOARD_SIZE, 4])).toEqual(false);
+    expect(testBoard.placeShip([0, BOARD_SIZE], [4, BOARD_SIZE])).toEqual(false);
+  });
+  test("ships cannot be placed diagonally", () => {
+    const testBoard = new Gameboard();
+    expect(testBoard.placeShip([0, 0], [1, 1])).toEqual(false);
+  });
+  test("ships cannot be placed on squares already containing a ship", () => {
+    const testBoard = new Gameboard();
+    testBoard.placeShip([0, 2], [0, 3]);
+    expect(testBoard.placeShip([0, 0], [0, 4])).toEqual(false);
+  });
+  test("ship coords can be given in any order", () => {
+    const boardOne = new Gameboard();
+    const boardTwo = new Gameboard();
+
+    boardOne.placeShip([0, 0], [0, 2]);
+    boardTwo.placeShip([0, 2], [0, 0]);
+
+    boardOne.placeShip([0, 0], [2, 0]);
+    boardTwo.placeShip([2, 0], [0, 0]);
+    expect(boardOne.board).toEqual(boardTwo.board);
+  });
 });
